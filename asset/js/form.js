bd67c79e9024006c2770e12a26a64300903aa3cd
Hazira Azza Faroni <60853582+azzabuza@users.noreply.github.com>
2025-01-24 15:32:55 +0700

    Update form.js

--- asset/js/form.js
+++ asset/js/form.js
@@ -184,7 +184,7 @@ paymentInfo.innerHTML = `
 <path d="M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z" />
 <path d="M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1" />
 </svg></p>
-<p>Salin kode transaksi lalu kirim melalui tombol konfirmasi agar transaksi segera diproses.</p>
+<p>Salin kode transaksi lalu simpan ke catatan jika suatu saat diperlukan.</p>
 <div class="data-payment">
 <table>
 <tr><td style="font-weight:500;">Penjual</td><td>${formData.get("Nama Penjual")}</td></tr>
